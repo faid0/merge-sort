32e425f138bcb1def0f062a63a345d37adea37f6
faid0 <faid.r03@gmail.com>
2024-04-19 00:09:48 +0200

    better tests

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,12 @@ import java.util.Random;
 public class Main {
     public static void main(String[] args) {
         int failed = 0;
-        for (int j = 0; j < 9999999; j++) {
-            int[] randarray = new int[10];
+        for (int j = 0; j < 100; j++) {
             Random rand = new Random();
+            int randIndex = rand.nextInt(0,999999);
+            int[] randarray = new int[randIndex];
             for (int i = 0; i < randarray.length; i++) {
-                randarray[i] = rand.nextInt(Integer.MAX_VALUE);
+                randarray[i] = rand.nextInt();
             }
 
             MergeSort a = new MergeSort();
