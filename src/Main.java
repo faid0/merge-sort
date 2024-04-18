6def2a8ff0a96c0b55a7d871efec82886d1c4b8a
faid0 <faid.r03@gmail.com>
2024-04-18 23:22:40 +0200

    simple main file to test the sorting

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int failed = 0;
+        for (int j = 0; j < 100; j++) {
+
+            int[] randarray = new int[100];
+            Random rand = new Random();
+            for (int i = 0; i < randarray.length; i++) {
+                randarray[i] = rand.nextInt(Integer.MAX_VALUE);
+            }
+            MergeSort a = new MergeSort();
+            int[] res = a.recursiveMergeSort(randarray, 0, randarray.length);
+            for (int i = 0; i < res.length - 1; i++) {
+                if (res[i] <= res[i + 1]) continue;
+                else {
+                    failed++;
+                    break;
+                }
+            }
+        }
+            System.out.println("failed: " + failed);
     }
 }
