801852d6800742c7cef5399a07b2450853cb060b
faid0 <faid.r03@gmail.com>
2024-04-18 23:38:05 +0200

    improved main test run

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,21 @@
+import java.util.Arrays;
 import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
         int failed = 0;
-        for (int j = 0; j < 100; j++) {
-
-            int[] randarray = new int[100];
+        for (int j = 0; j < 9999999; j++) {
+            int[] randarray = new int[10];
             Random rand = new Random();
             for (int i = 0; i < randarray.length; i++) {
                 randarray[i] = rand.nextInt(Integer.MAX_VALUE);
             }
+
             MergeSort a = new MergeSort();
             int[] res = a.recursiveMergeSort(randarray, 0, randarray.length);
-            for (int i = 0; i < res.length - 1; i++) {
-                if (res[i] <= res[i + 1]) continue;
-                else {
-                    failed++;
-                    break;
-                }
-            }
+            if(!Arrays.equals(res, Arrays.stream(randarray).sorted().toArray()))
+                failed++;
         }
-            System.out.println("failed: " + failed);
+        System.out.println("failed: " + failed);
     }
 }
