64734a0df3c032f04c5f6eb07b85d2752ca21863
faid0 <faid.r03@gmail.com>
2024-04-18 23:22:03 +0200

    implemented simple recursive merge sort

---
+++ src/MergeSort.java
@@ -0,0 +1,40 @@
+public class MergeSort {
+
+    // recursive merge sort algorithm
+    public int[] recursiveMergeSort(int[] numbers, int from, int to){
+        if(to-from == 1) return  new int[]{numbers[from]};
+        int half = (from + to) / 2;
+        int[] a = recursiveMergeSort(numbers, from, half);
+
+        int[] b = recursiveMergeSort(numbers, half, to);
+        int indexA = 0;
+        int indexB = 0;
+        int i = to-from;
+        int[] res = new int[i];
+        int index = 0;
+        while(index < i){
+            if(indexA == a.length){
+                res[index] = b[indexB];
+                indexB++;
+            }else if(indexB == b.length){
+                res[index] = a[indexA];
+                indexA++;
+            }else{
+                if(a[indexA] <= b[indexB]){
+                    res[index] = a[indexA];
+                    indexA++;
+                }else{
+                    res[index] = b[indexB];
+                    indexB++;
+                }
+            }
+            index++;
+        }
+        return res;
+    }
+
+    public void threadedMergeSort(){
+        
+    }
+
+}
